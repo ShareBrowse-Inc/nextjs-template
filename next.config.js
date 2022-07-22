cf9572a6866275a0d7611d742576cae22de2d79a
nishidayoshikatsu <nsd24technology@gmail.com>
2022-07-22 16:13:24 +0900

    :wrench: bundle-analyzerの設定を追加

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const nextConfig = {
   swcMinify: true,
 };
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 module.exports = nextConfig;
+module.exports = withBundleAnalyzer({});
