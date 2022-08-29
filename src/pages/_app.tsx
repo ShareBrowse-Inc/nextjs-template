f62fad25af2ed27b00cc580490743f75e78d97fe
nishidayoshikatsu <nsd24technology@gmail.com>
2022-08-29 18:13:19 +0900

    :mag: metaタグの説明文を修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,19 +9,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Next.js Template</title>
         <meta name="robots" content="noindex" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          href="https://experiment.liveblocks.sharebrowse.tech/favicon-32x32.png"
-          rel="icon"
-          sizes="32x32"
-          type="image/png"
+        <meta
+          name="description"
+          content="我々は「オンラインコミュニケーションの可能性をハックする」をミッションに掲げ、オンラインコミュニケーションの可能性をあらゆるテクノロジーとアイデアでハックし、世界中の人々に愛されるプロダクトをつくります。"
         />
-        <meta name="description" content="みんなでワイワイ🙌" />
         <meta property="og:url" content="https://experiment.liveblocks.sharebrowse.tech" />
-        <meta property="og:title" content="ShareBrowse" />
-        <meta property="og:site_name" content="ShareBrowse" />
+        <meta property="og:title" content="ShareBrowse,Inc." />
+        <meta property="og:site_name" content="ShareBrowse,Inc." />
         <meta
           property="og:description"
-          content="ShareBrowseは、チームメンバと同じコンテンツを見ている際に自動的にマウスカーソルを共有し、雑談の起点を創出します。"
+          content="我々は「オンラインコミュニケーションの可能性をハックする」をミッションに掲げ、オンラインコミュニケーションの可能性をあらゆるテクノロジーとアイデアでハックし、世界中の人々に愛されるプロダクトをつくります。"
         />
         <meta property="og:type" content="website" />
         <meta
