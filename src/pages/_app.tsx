02099c16b692a8727cae122202d2b2170dfeb939
nishidayoshikatsu <nsd24technology@gmail.com>
2022-08-31 05:25:43 +0900

    :mag: next-seoの設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import { useLocale } from "@/hooks/useLocale";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { t } = useLocale();
@@ -10,32 +11,50 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>{t.TITLE}</title>
         <meta name="robots" content="noindex" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={t.DESCRIPTION} />
 
-        <meta property="og:url" content={t.URL} />
-        <meta property="og:title" content={t.TITLE} />
-        <meta property="og:site_name" content={t.TITLE} />
-        <meta property="og:description" content={t.DESCRIPTION} />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/ogp.png" />
         <meta property="og:locale" content={t.LOCALE} />
         <meta property="og:locale:alternate" content={t.ALT_LOCALE} />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#faa603" />
         <meta name="theme-color" content="#faa603" />
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:creator" content={t.TWITTER} />
-        <meta property="fb:app_id" content={t.FACEBOOK} />
-
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
+      <DefaultSeo
+        titleTemplate={`%s | ${t.TITLE}`}
+        title={undefined}
+        defaultTitle={t.TITLE}
+        description={t.DESCRIPTION}
+        canonical={t.URL}
+        twitter={{
+          handle: t.TWITTER,
+          //site: t.TWITTER,
+          cardType: "summary_large_image",
+        }}
+        facebook={{
+          appId: t.FACEBOOK,
+        }}
+        openGraph={{
+          url: t.URL,
+          type: "website",
+          title: t.TITLE,
+          description: t.DESCRIPTION,
+          images: [
+            {
+              url: "/ogp.png",
+              width: 839,
+              height: 440,
+              alt: "ShareBrowse",
+            },
+          ],
+          locale: t.LOCALE,
+        }}
+      />
       <Component {...pageProps} {...t} />
     </>
   );
