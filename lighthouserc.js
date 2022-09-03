5028424fb96da8579b7c49941f036fe0cbbd53b4
nishidayoshikatsu <nsd24technology@gmail.com>
2022-09-04 03:39:51 +0900

    :wrench: lighthouseの起動サーバーを変更

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,8 +2,8 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 1,
-      startServerCommand: "yarn start",
-      url: ["http://localhost:3000/"],
+      startServerCommand: "yarn start 8081",
+      url: ["http://localhost:8081/"],
     },
     upload: {
       target: "temporary-public-storage",
