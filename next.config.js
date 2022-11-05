948643430902940e3316796e612634d65f3e2333
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2022-11-06 03:35:48 +0900

    :wrench: update i18n config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const { i18n } = require("./next-i18next.config");
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n,
+  i18n: {
+    locales: ["en", "ja"],
+    defaultLocale: "en",
+  },
   output: "standalone",
 };
 
